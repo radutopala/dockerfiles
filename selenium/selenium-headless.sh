7a7e1307c957d0dfbaae24e132e1f0f6ebe3c5c8
Viorel Craescu <viorel@craescu.com>
2016-08-14 07:44:25 +0300

    improvements

--- selenium/selenium-headless.sh
+++ selenium/selenium-headless.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+function shutdown {
+  kill -s SIGTERM $JAVA_PID
+  wait $JAVA_PID
+}
+
 echo "Starting Selenium Server Headless..."
-xvfb-run --server-args="-screen 0 1920x1080x8" java -jar /opt/selenium/selenium.jar -host 0.0.0.0 -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver
+
+Xvfb $DISPLAY -screen 0 $GEOMETRY -nolisten tcp & XVFB_PID=$!
+java -jar /opt/selenium/selenium.jar -host 0.0.0.0 -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver & JAVA_PID=$!
+trap shutdown SIGTERM SIGINT
+wait $JAVA_PID
+
+# xvfb-run --server-args="-screen 0 1920x1080x8" java -jar /opt/selenium/selenium.jar -host 0.0.0.0 -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver
 
