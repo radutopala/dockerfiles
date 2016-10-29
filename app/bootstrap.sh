91ee7f3002a1a1f859c20877116a6c1bb4d71be3
Viorel Craescu <viorel@craescu.com>
2016-10-29 11:22:50 +0300

    mongo & xdebug added

--- app/bootstrap.sh
+++ app/bootstrap.sh
@@ -5,5 +5,7 @@ setfacl -R -m u:"www-data":rwX -m u:`whoami`:rwX /dev/shm
 setfacl -dR -m u:"www-data":rwX -m u:`whoami`:rwX /dev/shm
 
 # start php-fpm
-/usr/sbin/php-fpm7.0 -F -O
+# /usr/sbin/php-fpm7.0 -F -O
+/usr/sbin/php-fpm7.0
+tail -f /dev/null
 
